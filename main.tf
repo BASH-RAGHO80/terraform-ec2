2a811a538f194dbe75921767c6b4e0ca9eaf6774
Basveshwar Ragho <raghobasveshwar@gmail.com>
2026-02-13 15:18:16 +0530

    Update AWS instance key name and tag

--- main.tf
+++ main.tf
@@ -4,9 +4,9 @@ provider "aws" {
 resource "aws_instance" "demo" {
   ami = ami-0b6c6ebed2801a5cb
   instance_type = "t3.micro"
-  key_name = "BashKey"
+
 tags = {
-    Name = "demo"
+    Name = "demo1"
 
     }
 }
