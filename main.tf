e43d1e0328cba638f4119cb2ef14855680a897c9
Basveshwar Ragho <raghobasveshwar@gmail.com>
2026-02-17 14:29:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,66 +1,17 @@
+# create a vpc in 
 provider "aws" {
   region = "us-east-1"
 }
-resource "aws_vpc" "my_vpc" {
-  cidr_block = "10.0.0.0/16"
-
-  tags = {
-    Name = "terraform-vpc"
-  }
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
 }
-resource "aws_subnet" "subnet1" {
-  vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
-
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+  enable_dns_hostnames = true
+  enable_dns_support = true
   tags = {
-    Name = "subnet-1"
-  }
-}
-
-resource "aws_subnet" "subnet2" {
-  vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = "10.0.2.0/24"
-  availability_zone = "us-east-1b"
-
-  tags = {
-    Name = "subnet-2"
-  }
-}
-resource "aws_security_group" "lb_sg" {
-  name        = "alb-sg"
-  vpc_id      = aws_vpc.my_vpc.id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-resource "aws_lb" "my_alb" {
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.lb_sg.id]
-  subnets            = [
-    aws_subnet.subnet1.id,
-    aws_subnet.subnet2.id
-  ]
-
-  tags = {
-    Name = "terraform-alb"
+    Name = "main-vpc"
   }
 }
